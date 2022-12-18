bcdcb7dd2b6efd29753c1f3dafb212b2d753c6a9
kapet <kapet@kapet.de>
2022-12-18 22:00:42 +0100

    day 16 part two (after getting help)

--- 2022/16/onetwo.py
+++ 2022/16/onetwo.py
@@ -33,106 +33,51 @@ for valve in distance.keys():
 ########################### ONE
 
 # start iteration:
-#   30 min left (plus 1 min to 'open' the valve in AA)
+#   given time left (plus 1 min to 'open' the valve in AA)
 #   on node AA
 #   0 pressure released
 #   no nodes visited yet
-stack = [(31, 'AA', 0, set())]
-max_released = 0
-while stack:
-    time_remaining, valve, released, visited = stack.pop(0)
-    visited.add(valve)
 
-    # open this valve, calculate pressure release for remaining time
-    time_remaining -= 1
-    released += time_remaining * flowrate[valve]
+def allPaths(max_time):
+    stack = [(max_time+1, 'AA', 0, set())]
+    results = {}
+    while stack:
+        time_remaining, valve, released, visited = stack.pop(0)
+        visited.add(valve)
 
-    # move on to nodes that can be reached within available time
-    moving_on = False
-    for target,cost in distance[valve].items():
-        if target not in visited:
-            new_time_remaining = time_remaining - cost
-            if new_time_remaining > 1:
-                stack.append((new_time_remaining, target, released, visited.copy()))
-                moving_on = True
+        # open this valve, calculate pressure release for remaining time
+        time_remaining -= 1
+        released += time_remaining * flowrate[valve]
 
-    if not moving_on:
-        max_released = max(max_released, released)
+        # remember the best result for this set of visited nodes
+        visited_key = frozenset(visited)
+        results[visited_key] = max(results.get(visited_key, 0), released)
 
-print('one:', max_released)
+        # move on to nodes that can be reached within available time
+        for target,cost in distance[valve].items():
+            if target not in visited:
+                new_time_remaining = time_remaining - cost
+                if new_time_remaining > 1:
+                    stack.append((new_time_remaining, target, released, visited.copy()))
+
+    return results
+
+print('one:', max(allPaths(30).values()))
 
 ########################### TWO
 
-visited_optimum = {} # visited set -> best release
+# adopted from juanplopes genius idea:
+# - calculate all possible one-player paths (including incomplete paths!) that fit in 26 minutes
+# - look for combinations of paths that do not both try to open the same valves, take the max
 
-# start iteration:
-#   26 min left (plus 1 min to 'open' the valve in AA)
-#   both on node AA with no remaining waiting time
-#   0 pressure released
-#   no nodes visited yet
-stack = [(26+1, ('AA', 0), ('AA', 0), 0, set([]))]
+possible_paths = allPaths(26)
+
 max_released = 0
-last_min = 0
-while stack:
-    time_remaining, p1, p2, released, visited = stack.pop(0)
-    p1_valve, p1_wait = p1
-    p2_valve, p2_wait = p2
-
-    if time_remaining != last_min:
-        print('   ', time_remaining, len(stack), len(visited_optimum))
-        last_min = time_remaining
-
-    time_remaining -= 1
-
-    if p1_wait == 0:
-        visited.add(p1_valve)
-        released += time_remaining * flowrate[p1_valve]
-        p1_next = []
-        for target, cost in distance[p1_valve].items():
-            if target != p2_valve and target not in visited and time_remaining - cost > 1:
-                p1_next.append((target, cost))
-        if not p1_next:
-            p1_next.append(('$$', 999))
-    else:
-        p1_next = [(p1_valve, p1_wait-1)]
-
-    if p2_wait == 0:
-        visited.add(p2_valve)
-        released += time_remaining * flowrate[p2_valve]
-        p2_next = []
-        for target, cost in distance[p2_valve].items():
-             if target != p1_valve and target not in visited and time_remaining - cost > 1:
-                p2_next.append((target, cost))
-        if not p2_next:
-            p2_next.append(('$$', 999))
-    else:
-        p2_next = [(p2_valve, p2_wait-1)]
-
-    visited_key = tuple(sorted(visited))
-    if released < visited_optimum.get(visited_key, 0):
-        # there's another path that achieved bigger release with the same set of nodes visited
-        # drop this path
-        continue
-    visited_optimum[visited_key] = released
-
-    taken = set()
-    for p1, p2 in itertools.product(p1_next, p2_next):
-        if (p2, p1) in taken:
-            # already took this same path with p1/p2 swapped
-            continue
-        if p1[0] == p2[0]:
-            if p1[0] == '$$':
-                # done
-                if released > max_released:
-                    print('new max:', visited, released)
-                max_released = max(max_released, released)
-                continue
-            else:
-                # no sense for both players going to the same destination if we're not finished
-                # risk: what if only one node remains and BOTH just finished a node?
-                continue
-
-        taken.add((p1, p2))
-        stack.append((time_remaining, p1, p2, released, visited.copy()))
+for path1, released1 in possible_paths.items():
+    path1 = set(path1)
+    path1.remove('AA')
+    for path2, released2 in possible_paths.items():
+        if path1.isdisjoint(path2):
+            max_released = max(max_released, released1+released2)
 
 print('two:', max_released)
