dc45f8d7389029699b7e312cb00737d404c28eb0
kapet <kapet@kapet.de>
2023-01-08 21:27:55 +0100

    day 17 part two - final part done \o/

--- 2022/17/onetwo.py
+++ 2022/17/onetwo.py
@@ -1,7 +1,7 @@
 input_test = '>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>'
 input_full = open('2022/17/input.txt').read().strip()
 jets = input_full
-jets_index = 0
+jets_index = -1
 
 rocks = [
     (0b00111100,),
@@ -10,24 +10,30 @@ rocks = [
     (0b00100000, 0b00100000, 0b00100000, 0b00100000),
     (0b00110000, 0b00110000)
 ]
-rocks_index = 0
+rocks_index = -1
 
 height = 1  # line 0 is the 'ground'
 chamber = [0b111111111]  # 9 bits! 7 for the content, 1 each side for the wall
 rock = None
 number_of_rocks = 0
+
+series = []
+jets_iterations = 0
+
 while True:
     if not rock:
-        rock = rocks[rocks_index]
         rocks_index = (rocks_index+1) % len(rocks)
+        rock = rocks[rocks_index]
 
         rock_y = height + 3
         for i in range(rock_y + len(rock) - len(chamber)):
             chamber.append(0b100000001)
     
     # STEP 1: rock being pushed by jet
-    jet = jets[jets_index]
     jets_index = (jets_index+1) % len(jets)
+    if jets_index == 0:
+        jets_iterations += 1
+    jet = jets[jets_index]
     assert jet in '<>'
     if jet == '<':
         shift = int.__lshift__
@@ -51,6 +57,7 @@ while True:
                 chamber[rock_y+j] |= rock[j]
             height = max(height, rock_y + len(rock))
             rock = None
+            series.append((number_of_rocks+1, rocks_index, jets_index, height-1))
             break
     else:
         # free to go one down
@@ -68,6 +75,48 @@ while True:
         number_of_rocks += 1
         if number_of_rocks == 2022:
             print('one:', height-1)
+
+        if number_of_rocks > 1000000 or number_of_rocks > 2022 and jets_iterations > 50:
             break
 
+print('saved {} rocks in {} jet iterations of {} each'.format(number_of_rocks, jets_iterations, len(jets)))
 
+# cycle detection using Floyd's algorithm
+def key(i):
+    _, ri, ji, h = series[i]
+    h -= series[i-len(jets)][3]
+    return (ri, ji, h)
+
+tortoise = len(jets)
+hare = len(jets)*2
+while key(tortoise) != key(hare):
+    tortoise += 1
+    hare += 2
+print('found a cycle at: {} / {}'.format(tortoise, hare))
+
+mu = 0
+tortoise = 0
+while key(tortoise) != key(hare):
+    tortoise += 1
+    hare += 1
+    mu += 1
+print('found start of cycle at:', mu)
+
+lam = 1
+hare = tortoise + 1
+while key(tortoise) != key(hare):
+    hare += 1
+    lam += 1
+print('found minimal length of cyle as:', lam)
+
+cycle_blocks = series[mu+lam][0] - series[mu][0]
+cycle_growth = series[mu+lam][3] - series[mu][3]
+base_blocks = series[mu][0]
+base_height = series[mu][3]
+
+target_blocks = 1000000000000
+cycles,rest_blocks = divmod(target_blocks - base_blocks, cycle_blocks)
+remaining_height = series[mu+rest_blocks][3] - series[mu][3]
+total_height = base_height + cycles*cycle_growth + remaining_height
+
+print('two:', total_height)
