09048c23d1a5f3b319e72cf1547eab13a2544120
kapet <kapet@kapet.de>
2023-12-16 21:18:35 +0100

    optimization attempts for day 16
    speed up about factor 3
    
    more probably only possible with numpy or using some completely different algorithm

--- 2023/16/onetwo.py
+++ 2023/16/onetwo.py
@@ -2,31 +2,53 @@ data = [list(l.strip()) for l in open('2023/16/input.txt')]
 width = len(data[0])
 height = len(data)
 
+lookup_reverse = [0, 2, 1, 0, 8, 0, 0, 0, 4]
+lookup_nwse = [0, 4, 8, 0, 1, 0, 0, 0, 2]   # \
+lookup_nesw = [0, 8, 4, 0, 2, 0, 0, 0, 1]   # /
+
 def raytrace(starty, startx, stepy, stepx):
     beams = [(starty, startx, stepy, stepx)]
-    completed = set()
+    marked = 0
+    completed = [0]*width*height
     while beams:
-        posy, posx, stepy, stepx = quad = beams.pop(0)
-        if posy < 0 or posx < 0 or posy >= height or posx >= width:
-            continue
-        if quad in completed:
-            continue
-        completed.add(quad)
-        match data[posy][posx]:
-            case '/':
-                stepy,stepx = -stepx,-stepy
-            case '\\':
-                stepy,stepx = stepx,stepy
-            case '-':
-                if stepy:
-                    beams.append((posy, posx-1, 0, -1))
-                    stepy,stepx = 0,1
-            case '|':
-                if stepx:
-                    beams.append((posy-1,posx,-1,0))
-                    stepy,stepx = 1,0
-        beams.append((posy+stepy, posx+stepx, stepy, stepx))
-    return len(set((y, x) for y, x, _, _ in completed))
+        posy, posx, stepy, stepx = beams.pop()
+        key = stepy and (stepy<0 and 1 or 2) or (stepx<0 and 4 or 8)
+        while posy >= 0 and posx >= 0 and posy < height and posx < width:
+            comp = completed[posy*width + posx]
+            if not comp:
+                marked += 1
+                comp = key
+            elif comp & key:
+                break
+            else:
+                comp |= key
+            match data[posy][posx]:
+                case '.':
+                    comp |= lookup_reverse[key]
+                case '/':
+                    stepy,stepx = -stepx,-stepy
+                    key = lookup_nesw[key]
+                    comp |= lookup_reverse[key]
+                case '\\':
+                    stepy,stepx = stepx,stepy
+                    key = lookup_nwse[key]
+                    comp |= lookup_reverse[key]
+                case '-':
+                    comp |= lookup_reverse[key]
+                    if stepy:
+                        beams.append((posy, posx-1, 0, -1))
+                        stepy,stepx = 0,1
+                        key = 8
+                case '|':
+                    comp |= lookup_reverse[key]
+                    if stepx:
+                        beams.append((posy-1,posx,-1,0))
+                        stepy,stepx = 1,0
+                        key = 2
+            completed[posy*width+posx] = comp
+            posy += stepy
+            posx += stepx
+    return marked
 
 print('one:', raytrace(0, 0, 0, 1))
 
