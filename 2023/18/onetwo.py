017fbac4478a7ffb8c9a65ba0c61e2c5d2776e1b
kapet <kapet@kapet.de>
2023-12-18 17:46:27 +0100

    day 18

--- 2023/18/onetwo.py
+++ 2023/18/onetwo.py
@@ -1,58 +1,50 @@
-plan = [(l[0], int(l[1]), l[2][1:-1]) for l in (line.split() for line in open('2023/18/test.txt'))]
+data = [line.split() for line in open('2023/18/input.txt')]
 
-coords = []
-posy = posx = 0
-miny = minx = 0
-maxx = maxy = 0
-for direction, distance, _ in plan:
-    oldy, oldx = posy, posx
-    match direction:
-        case 'R':
-            posx += distance
-        case 'D':
-            posy += distance
-        case 'L':
-            posx -= distance
-        case 'U':
-            posy -= distance
-    miny = min(miny, posy)
-    minx = min(minx, posx)
-    maxy = max(maxy, posy)
-    maxx = max(maxx, posx)
-    coords.append(((oldy, oldx), (posy, posx)))
-coords = [((c[0][0]-miny,c[0][1]-minx),(c[1][0]-miny,c[1][1]-minx)) for c in coords]
-width = maxx-minx+1
-height = maxy-miny+1
+plan_one = []
+plan_two = []
+for line in data:
+    plan_one.append((line[0], int(line[1])))
 
-def sign(x):
-    return (x > 0) - (x < 0)
+    color = line[2]
+    distance = int(color[2:7], 16)
+    direction = 'RDLU'[int(color[7])]
+    plan_two.append((direction, distance))
 
-field = [['.']*width for _ in range(height)]
-for (sy,sx), (ey,ex) in coords:
-    iy = sign(ey-sy)
-    ix = sign(ex-sx)
-    while (sy,sx) != (ey, ex):
-        sy += iy
-        sx += ix
-        field[sy][sx] = '#'
+def to_coordinates(plan):
+    coords = []
+    posy = posx = 0
+    for direction, distance in plan:
+        oldy, oldx = posy, posx
+        match direction:
+            case 'R':
+                posx += distance
+            case 'D':
+                posy += distance
+            case 'L':
+                posx -= distance
+            case 'U':
+                posy -= distance
+        coords.append(((oldy, oldx), (posy, posx)))
+    return coords
 
-posy = 1
-posx = field[posy].index('#') + 1
-heap = [(posy, posx)]
-while heap:
-    posy, posx = heap.pop()
-    if field[posy][posx] == '#':
-        continue
-    field[posy][posx] = '#'
-    for y,x in ((posy-1,posx),(posy+1,posx),(posy,posx-1),(posy,posx+1)):
-        if field[y][x] == '.':
-            heap.append((y,x))
+def calc_pixel_area(coords):
+    # The coordinates represent a polygon.
+    # Use Shoelace formula to calculate surface area.
+    # This gives wrong result because of fence post issue with the pixels.
+    # To correct: Think of polygon outline being in the center of the pixels.
+    # Now every step on a line misses 1/2 pixel, so that means add half of the
+    # total outline of the polygon. And every corner either misses or gains
+    # 1/4 pixel. But corners balance each other out, except for four corners
+    # that each miss 1/4 pixel -> add one extra pixel.  
+    result = 0
+    outline = 0
+    for (sy,sx),(ey,ex) in coords:
+        outline += abs(ey-sy)+abs(ex-sx)
+        result += (sy+ey)*(sx-ex)
+    return abs(result//2) + outline//2 + 1
 
-print('one:', sum(l.count('#') for l in field))
+coords_one = to_coordinates(plan_one)
+print('one:', calc_pixel_area(coords_one))
 
-newplan = []
-for _, _, color in plan:
-    distance = int(color[1:6], 16)
-    direction = 'RDLU'[int(color[6])]
-    newplan.append((direction, distance))
-print(newplan)
+coords_two = to_coordinates(plan_two)
+print('two:', calc_pixel_area(coords_two))
