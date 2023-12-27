1e1c973a3eb94ee01f2cc2153868711af2f7359d
kapet <kapet@kapet.de>
2023-12-27 17:18:27 +0100

    day 23 part two, finally!
    reimplemented the whole thing from a direct search on the maze to first converting the maze into a graph so that the exhaustive DFS can run much faster

--- 2023/23/onetwo.py
+++ 2023/23/onetwo.py
@@ -1,71 +1,153 @@
 import collections
+import dataclasses
 
-maze = [line.strip() for line in open('2023/23/test.txt')]
-height = len(maze)
-width = len(maze[0])
+@dataclasses.dataclass(frozen=True)
+class Vector:
+    y: int
+    x: int
 
-starty, startx = 0, 1
-endy, endx = height-1, width-2
-assert maze[starty][startx] == maze[endy][endx] == '.'
+    def __add__(self, other):
+        return Vector(self.y+other.y, self.x+other.x)
 
-def longest_path(possible_steps):
-    visited_root = collections.ChainMap()
-    max_distances = [[-1]*width for _ in range(height)]
+_maze = [line.strip() for line in open('2023/23/input.txt')]
+def maze(loc):
+    return _maze[loc.y][loc.x]
+height = len(_maze)
+width = len(_maze[0])
+
+start = Vector(0, 1)
+end = Vector(height-1, width-2)
+assert maze(start) == maze(end) == '.'
+
+directions = '><v^'
+possible_steps = (
+    (Vector( 0, 1), '>'),
+    (Vector( 0,-1), '<'),
+    (Vector( 1, 0), 'v'),
+    (Vector(-1, 0), '^')
+)
+
+# STEP 1: convert maze into a graph
+# The maze has many steps between few junctions, so this simplifies the later
+# path finding work.
+nodes = set([start])
+edges = set()
+to_visit = [(start, start+Vector(1, 0))]
+while to_visit:
+    first, pos = to_visit.pop(0)
+    steps = 0
+    previous = first
+    blocked_forward = blocked_backwards = False
+
+    while True:
+        steps += 1
+        if pos == end:
+            assert not blocked_backwards
+            edges.add((first, pos, steps, blocked_forward))
+            break
+
+        next_steps = []
+        for inc, direction in possible_steps:
+            new = pos + inc
+            if maze(new) != '#' and new != previous:
+                next_steps.append((new, direction))
+        if not next_steps:
+            # dead end!
+            break
+        if len(next_steps) > 1:
+            # found a new node
+            if blocked_backwards:
+                edges.add((pos, first, steps, True))
+            else:
+                edges.add((first, pos, steps, blocked_forward))
+            if pos not in nodes:
+                for new, _ in next_steps:
+                    to_visit.append((pos, new))
+                nodes.add(pos)
+            break
+        else:
+            # path continues
+            new, direction = next_steps[0]
+            if maze(pos) in directions:
+                if maze(pos) == direction:
+                    blocked_forward = True
+                else:
+                    blocked_backwards = True
+            previous, pos = pos, new
+
+# STEP 2: graph cleanup
+
+# Convert set of nodes into list, ensure start is first and end is last.
+# Will use the index in this list as the node ID, for speed reasons.
+nodes = list(nodes)
+nodes.remove(start)
+nodes.insert(0, start)
+nodes.append(end)
+
+start = nodes.index(start)
+end = nodes.index(end)
+assert start == 0
+assert end == len(nodes)-1
+
+# Build complete outgoing edge lists for all nodes. 
+undirected_graph = collections.defaultdict(list)
+directed_graph = collections.defaultdict(list)
+for src, dest, steps, oneway in edges:
+    src = nodes.index(src)
+    dest = nodes.index(dest)
+    undirected_graph[src].append((dest, steps))
+    undirected_graph[dest].append((src, steps))
+    directed_graph[src].append((dest, steps))
+    if not oneway:
+        directed_graph[dest].append((src, steps))
+
+assert len(undirected_graph[start]) == 1
+assert len(directed_graph[start]) == 1
+
+# STEP 3: calculate longest path using exhaustive DFS
+# Optimized for speed as the problem is IIUC NP-hard.
+
+def longest_path(graph):
+    visited_root = set()  # ChainMap is more elegant but much slower
     heap = []
-    
-    heap.append((1, starty+1, startx, visited_root))
-    visited_root[(starty, startx)] = True
-    max_distances[starty][startx] = 0
 
+    # There can only be one edge that leaves start, so actually
+    # begin at the next node.
+    pos, steps = graph[start][0]
+    heap.append((pos, steps, visited_root))
+    visited_root.add(start)
+
+    # There can only be one edge that reaches the end, so only try to
+    # get to this node.
+    for k,v in graph.items():
+        for n,s in v:
+            if n == end:
+                pre_end, end_steps = k, s
+                break
+        else:
+            continue
+        break
+
+    maxlen = 0
     while heap:
-        print('####', len(heap))
-        # pop from the end -> DFS, priority to high number of steps
-        steps, posy, posx, visited = heap.pop() 
-        while True:
-            if max_distances[posy][posx] >= steps:
-                # have previously seen a better/longer path!
-                print('  STOP: cheap', steps, max_distances[posy][posx])
-                break
-            visited[(posy, posx)] = True
-            max_distances[posy][posx] = steps
-            if (posy, posx) == (endy, endx):
-                print('   STOP: end', steps)
-                break
-            if 1:
-                for y, line in enumerate(maze):
-                    out = []
-                    for x, c in enumerate(line):
-                        if (y,x) == (posy, posx):
-                            c = 'X'
-                        elif (y,x) in visited:
-                            c = 'o'
-                        out.append(c)
-                    print('          ' + ''.join(out))
-                print()
-            next_steps = []
-            current_field = maze[posy][posx]
-            for incy, incx, ok in possible_steps:
-                if current_field in ok:
-                    newy = posy+incy
-                    newx = posx+incx
-                    if maze[newy][newx] in ok and (newy, newx) not in visited:
-                        next_steps.append((newy, newx))
-            if not next_steps:
-                print('   STOP: stuck')
-                break
-            steps += 1
-            if len(next_steps) > 1:
-                for newy, newx in next_steps[1:]:
-                    heap.append((steps, newy, newx, visited.new_child()))
-                    visited = visited.new_child()
-            posy, posx = next_steps[0]
-    return max_distances[endy][endx]
+        pos, steps, visited = heap.pop()
+        visited.add(pos)
 
-#print('one:', longest_path([(0,1,'.>'), (0,-1,'.<'), (1,0,'.v'), (-1,0,'.^')]))
-print('two:', longest_path([(0,1,'.><v^'), (0,-1,'.><v^'), (1,0,'.><v^'), (-1,0,'.><v^')]))
+        for next_node, next_steps in graph[pos]:
+            if next_node in visited:
+                # this is very often true, checked first for speed reasons
+                pass
+            elif next_node == pre_end:
+                # as we may not visit the same node twice, we must go to the
+                # end once we visit the only pre-end node
+                maxlen = max(maxlen, steps+next_steps+end_steps)
+            else:
+                heap.append((next_node, steps+next_steps, visited.copy()))
+    return maxlen
 
-# not: 5006, too low
-
-if 0:
-    for line in maze:
-        print(line)
+import time
+t0 = time.time()
+print('one:', longest_path(directed_graph))
+print('two:', longest_path(undirected_graph))
+t1 = time.time()
+print('duration:', t1-t0)
